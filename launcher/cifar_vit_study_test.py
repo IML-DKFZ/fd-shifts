24208092c33831d7eb260ed0b7bbc1a8351b17d1
Till Bungert <till.bungert@dkfz-heidelberg.de>
2021-11-10 15:30:29 +0100

    Do validation

--- launcher/cifar_vit_study_test.py
+++ launcher/cifar_vit_study_test.py
@@ -15,18 +15,22 @@ base_command = '''bsub \\
 # base_command = "EXPERIMENT_ROOT_DIR=~/cluster/experiments DATASET_ROOT_DIR=~/Data python -W ignore {} {}"
 
 # datasets = ["cifar10", "cifar100"]
-runs = range(1, 5)
-datasets = ["svhn", "breeds", "wilds_camelyon"]
-lrs = [0.003, 0.003, 0.003]
-for run, (dataset, lr) in product(runs, zip(datasets, lrs)):
+runs = range(1)
+datasets = ["cifar10", "cifar100", "svhn", "breeds", "wilds_camelyon", "wilds_animals"]
+lrs = [1e-2, 3e-2, 1e-3, 3e-3, 1e-4, 3e-4]
+for run, dataset, lr in product(runs, datasets, lrs):
     command_line_args = ""
     command_line_args += "study={}_vit_study ".format(dataset)
     command_line_args += "exp.name={}_lr{}_run{} ".format(dataset, lr, run)
     command_line_args += "exp.mode={} ".format("test")
     # command_line_args += "exp.mode={} ".format("analysis")
     command_line_args += "trainer.learning_rate={} ".format(lr)
+    command_line_args += "trainer.val_split=devries "
+    command_line_args += "test.name=validation "
     command_line_args += "+trainer.do_val=true "
-    command_line_args += "+eval.val_tuning=false "
+    command_line_args += "+eval.val_tuning=true "
+    command_line_args += "+model.dropout_rate=0 "
+    command_line_args += "eval.r_star=0.15 "
     command_line_args += "trainer.batch_size=128 "
 
     launch_command = base_command.format(exec_path, command_line_args)
