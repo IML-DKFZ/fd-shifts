769878e54624c8e91b89cbd49ec4d85161cb7e30
Till Bungert <tillbungert@gmail.com>
2021-12-09 21:01:30 +0100

    fix: add confidnet to test gathering

--- gather_tests.py
+++ gather_tests.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+from itertools import product
+from typing import Optional
+import re
 import pandas as pd
 from rich import print
-from rich.table import Table
-import re
 
 
 datasets = [
@@ -21,6 +22,55 @@ experiments = [
     "vit_dg",
     "vit_confidnet",
 ]
+
+
+def check_missing_tests():
+    rewards = [2.2, 3, 4.5, 6, 10]
+    exps: list[tuple[list, list, list, list, list, list, list, range, Optional[list]]] = [
+        (["cifar10"], ["confidnet"], ["vit"], [0.01], [256 // 2], [1], [2.2], range(1), [2]),
+        (["breeds"], ["dg"], ["vit"], [0.01], [128], [1], rewards, range(1), [None]),
+        (["svhn"], ["dg"], ["vit"], [0.01], [128], [1], rewards, range(1), [None]),
+        (["wilds_camelyon"], ["dg"], ["vit"], [0.003], [128], [1], rewards, range(1), [None]),
+        (["cifar10"], ["devries"], ["vit"], [0.0003], [128], [0], [2.2], range(1), [None]),
+        (["cifar10"], ["devries"], ["vit"], [0.01], [128], [1], [2.2], range(1), [None]),
+        (["breeds"], ["devries"], ["vit"], [0.001], [128], [0], [2.2], range(1), [None]),
+        (["breeds"], ["devries"], ["vit"], [0.01], [128], [1], [2.2], range(1), [None]),
+        (["svhn"], ["devries"], ["vit"], [0.01], [128], [0], [2.2], range(1), [None]),
+        (["svhn"], ["devries"], ["vit"], [0.01], [128], [1], [2.2], range(1), [None]),
+        (["wilds_camelyon"], ["devries"], ["vit"], [0.001], [128], [0], [2.2], range(1), [None]),
+        (["wilds_camelyon"], ["devries"], ["vit"], [0.003], [128], [1], [2.2], range(1), [None]),
+        (["cifar100"], ["dg"], ["vit"], [0.01], [512], [1], rewards, range(1), [None]),
+        (["wilds_animals"], ["dg"], ["vit"], [0.01], [512], [1], rewards, range(1), [None]),
+        (["cifar100"], ["devries"], ["vit"], [0.03], [512], [0], [2.2], range(1), [None]),
+        (["wilds_animals"], ["devries"], ["vit"], [0.001], [512], [0], [2.2], range(1), [None]),
+        (["cifar100"], ["devries"], ["vit"], [0.01], [512], [1], [2.2], range(1), [None]),
+        (["wilds_animals"], ["devries"], ["vit"], [0.01], [512], [1], [2.2], range(1), [None]),
+        (["super_cifar100"], ["dg"], ["vit"], [0.001], [128], [1], rewards, range(1), [None]),
+        (["super_cifar100"], ["devries"], ["vit"], [0.003], [128], [0], [2.2], range(1), [None]),
+        (["super_cifar100"], ["devries"], ["vit"], [0.001], [128], [1], [2.2], range(1), [None]),
+    ]
+
+    base_path = Path("~/cluster/experiments/vit").expanduser()
+
+    for experiment in exps:
+        for dataset, model, bb, lr, bs, do, rew, run, stage in product(*experiment):
+            exp_name = "{}_model{}_bb{}_lr{}_bs{}_run{}_do{}_rew{}".format(
+                dataset,
+                model,
+                bb,
+                lr,
+                bs,
+                run,
+                do,
+                rew,
+            )
+
+            if not (base_path / exp_name).exists():
+                print(f"[bold blue]{exp_name} not found, might not have been run at all (or uses old naming scheme)")
+                continue
+
+            if not (base_path / exp_name / "test_results").exists():
+                print(f"[bold red]{exp_name}/test_results not found")
 
 
 def rename(row):
@@ -86,6 +136,8 @@ def main():
                 row_confid = row_confid + "dg"
             elif "devries" in row.confid:
                 row_confid = row_confid + "devries"
+            elif "tcp" in row.confid:
+                row_confid = row_confid + "tcp"
             else:
                 row_confid = row_confid + "pe"
 
@@ -112,11 +164,13 @@ def main():
             | (selection_df.confid.str.contains("maha"))
             | (selection_df.confid.str.contains("dg"))
             | (selection_df.confid.str.contains("devries"))
+            | (selection_df.confid.str.contains("tcp"))
             & (~(selection_df.confid.str.contains("waic")))
         ].reset_index()
         selection_df.confid = selection_df.confid.str.replace("maha_mcd", "mcd_maha")
         selection_df.confid = selection_df.confid.str.replace("dg_mcd", "mcd_dg")
         selection_df.confid = selection_df.confid.str.replace("devries_mcd", "mcd_devries")
+        selection_df.confid = selection_df.confid.str.replace("tcp_mcd", "mcd_tcp")
         selection_df = selection_df.iloc[
             selection_df.groupby(["confid", "do"])[metric].idxmin()
         ]
@@ -149,18 +203,7 @@ def main():
 
         dataset = dataset.replace("wilds_", "")
         dataset = dataset.replace("cifar10_", "cifar10")
-        print(selected_df)
-
-        # # De Vries
-        # base_path = Path("~/cluster/experiments/").expanduser()
-        # csv2 = [pd.read_csv(p) for p in base_path.glob(f"devries_vit_{model}/*_run*/test_results/*.csv")]
-        # if len(csv2) > 0:
-        #     print(f"Found {len(csv2)} De Vries runs")
-        #     csv2 = pd.concat(csv2)
-        #     csv2["name"] = csv2.apply(lambda row: row["name"] + "_rew0", axis=1)
-        #
-        #     print(model)
-        #     csv = pd.concat([csv, csv2])
+        # print(selected_df)
 
         out_path = Path("~/Projects/failure-detection-benchmark/results").expanduser()
         selected_df.to_csv(out_path / f"{dataset}vit.csv")
@@ -168,3 +211,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # check_missing_tests()
