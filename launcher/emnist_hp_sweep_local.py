f886e33c054a0c8ca6329af2a982d427a31db7c7
Levin Kobelke <levin.kobelke@dkfz-heidelberg.de>
2022-05-09 14:20:38 +0200

    adding additional med_mnist datasets for multiclass problems + stronger regularization in med_mnist grid search launcher

--- launcher/emnist_hp_sweep_local.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-for lr in [1, 2, 3]:
-    for dropout in [0, 1]:
-        #print(f"{lr} and {dropout}")
-        learning_rate = 10**(-lr)
-        subprocess.run([f"fd_shifts data=emnist_data_balanced exp.group_name=test_emnist_hp_optim exp.name=emnist_balanced_hp_lr_e{lr}_drO_{dropout} eval.query_studies.iid_study=emnist_balanced eval.query_studies.new_class_study=[emnist_balanced] trainer.num_epochs=100 trainer.optimizer.learning_rate={learning_rate} model.dropout_rate={dropout}"], shell=True)
