f2dd892b1340bfc96f8d657420e37fe131acd6cd
Levin Kobelke <levin.kobelke@dkfz-heidelberg.de>
2022-05-13 09:29:33 +0200

    add target_transform in data_yaml

--- launcher/med_mnist_hp_sweep.py
+++ launcher/med_mnist_hp_sweep.py
@@ -14,7 +14,7 @@ for name in set:
                 exp_name = f"{name}_lr_e{lr}_drO_{dropout}_wgdc_e{wd}"
                 subprocess.run(
                     [
-                        f'bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=10.7G -L /bin/bash -q gpu "source ~/.bashrc && conda activate fd-shifts && fd_shifts data={data_name} exp.group_name={group_name} exp.name={exp_name} eval.query_studies.iid_study={data_name} eval.query_studies.new_class_study=[emnist_balanced] trainer.num_epochs=150 trainer.optimizer.learning_rate={learning_rate} model.dropout_rate={dropout} trainer.optimizer.weight_decay={weight_decay}"'
+                        f'bsub -gpu num=1:j_exclusive=yes:mode=exclusive_process:gmem=10.7G -L /bin/bash -q gpu "source ~/.bashrc && conda activate fd-shifts && fd_shifts data={data_name} exp.group_name={group_name} exp.name={exp_name} eval.query_studies.iid_study={data_name} eval.query_studies.new_class_study=[emnist_balanced] trainer.num_epochs=150 trainer.optimizer.learning_rate={learning_rate} model.dropout_rate={dropout} trainer.optimizer.weight_decay={weight_decay} trainer.do_val=True"'
                     ],
                     shell=True,
                 )
