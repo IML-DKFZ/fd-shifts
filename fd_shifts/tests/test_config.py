12d7ca073dd65a8c7672d02bf872088b6a3ab952
Till Bungert <tillbungert@gmail.com>
2022-06-10 15:36:54 +0200

    fix: tests now actually fully check config

--- fd_shifts/tests/test_config.py
+++ fd_shifts/tests/test_config.py
@@ -3,33 +3,66 @@ from typing import Optional
 
 import hydra
 import pytest
-from hydra import compose, initialize, initialize_config_module
+from hydra import compose, initialize_config_module
 from hydra.core.config_store import ConfigStore
+from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 from rich import print as pprint
 
-import fd_shifts.configs as configs
+from fd_shifts import configs
+
+# This would normally get set by hydra.main
+default_overrides = [
+    "hydra.job.num=0",
+    "hydra.job.id=0",
+    "hydra.runtime.output_dir=.",
+    "hydra.hydra_help.hydra_help=''",
+]
+
+
+def initialize_hydra(overrides: list[str]) -> DictConfig:
+    """Takes the place of hydra.main"""
+    with initialize_config_module(version_base=None, config_module="fd_shifts.configs"):
+        cfg = compose(
+            "config", overrides=overrides + default_overrides, return_hydra_config=True
+        )
+        HydraConfig.instance().set_config(cfg)
+
+    return cfg
 
 
 @pytest.mark.parametrize(
-    "overrides, expected",
+    "overrides",
     [
-        ([], None),
-        # pytest.param(["exp.log_path=test"], None, marks=pytest.mark.xfail),
-        (["exp.log_path=test"], None),
+        [],
+        ["exp.log_path=test"],
+        pytest.param(
+            ["trainer.val_split=foo"],
+            marks=pytest.mark.xfail(reason="foo is not a valid val_split"),
+        ),
     ],
 )
-def test_schema_with_old(overrides: list[str], expected: Optional[int]):
+def test_schema_with_old(overrides: list[str]):
+    configs.init()
 
-    store = ConfigStore.instance()
-    store.store(name="config_schema", node=configs.Config)
-    store.store(group="data", name="data_schema", node=configs.DataConfig)
+    cfg = initialize_hydra(overrides)
 
-    with initialize_config_module(version_base=None, config_module="fd_shifts.configs"):
-        cfg = compose("config", overrides=overrides)
-        pprint(OmegaConf.to_yaml(cfg))
-
-    # assert False
+    print(type(cfg))
+    cfg = OmegaConf.to_object(cfg)
+    pprint(OmegaConf.to_yaml(cfg, resolve=False))
+    print(type(cfg))
 
 
-# TODO: Write tests with weird configs
+# @hydra.main(config_path="../configs", config_name="config")
+# def main(cfg):
+#
+#     pprint(OmegaConf.to_yaml(cfg))
+#     OmegaConf.resolve(cfg)
+#     pprint(OmegaConf.to_yaml(cfg))
+#     OmegaConf.to_object(cfg)
+#     print("DONE")
+#
+#
+# if __name__ == "__main__":
+#     configs.init()
+#     main()
