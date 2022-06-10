bc3da0b4654a7c372f91cbad96b20584e175a82f
Till Bungert <tillbungert@gmail.com>
2022-06-10 16:31:16 +0200

    test: test all existing config yamls against schema

--- fd_shifts/tests/test_config.py
+++ fd_shifts/tests/test_config.py
@@ -11,17 +11,16 @@ from rich import print as pprint
 
 from fd_shifts import configs
 
-# This would normally get set by hydra.main
-default_overrides = [
-    "hydra.job.num=0",
-    "hydra.job.id=0",
-    "hydra.runtime.output_dir=.",
-    "hydra.hydra_help.hydra_help=''",
-]
-
 
 def initialize_hydra(overrides: list[str]) -> DictConfig:
     """Takes the place of hydra.main"""
+    default_overrides = [
+        "hydra.job.num=0",
+        "hydra.job.id=0",
+        "hydra.runtime.output_dir=.",
+        "hydra.hydra_help.hydra_help=''",
+    ]
+
     with initialize_config_module(version_base=None, config_module="fd_shifts.configs"):
         cfg = compose(
             "config", overrides=overrides + default_overrides, return_hydra_config=True
@@ -42,7 +41,7 @@ def initialize_hydra(overrides: list[str]) -> DictConfig:
         ),
     ],
 )
-def test_schema_with_old(overrides: list[str]):
+def test_validation(overrides: list[str]):
     configs.init()
 
     cfg = initialize_hydra(overrides)
@@ -53,16 +52,30 @@ def test_schema_with_old(overrides: list[str]):
     print(type(cfg))
 
 
-# @hydra.main(config_path="../configs", config_name="config")
-# def main(cfg):
-#
-#     pprint(OmegaConf.to_yaml(cfg))
-#     OmegaConf.resolve(cfg)
-#     pprint(OmegaConf.to_yaml(cfg))
-#     OmegaConf.to_object(cfg)
-#     print("DONE")
-#
-#
-# if __name__ == "__main__":
-#     configs.init()
-#     main()
+@pytest.mark.parametrize(
+    ("study",),
+    [
+        # ("breeds_vit_study",),
+        # ("cifar100_vit_study",),
+        ("cifar10_vit_study",),
+        # ("confidnet",),
+        ("deepgamblers",),
+        ("devries",),
+        # ("super_cifar100_vit_study",),
+        # ("svhn_openset_vit_study",),
+        # ("svhn_vit_study",),
+        # ("wilds_animals_openset_vit_study",),
+        # ("wilds_animals_vit_study",),
+        # ("wilds_camelyon_vit_study",),
+    ],
+)
+def test_existing_yamls(study: str):
+    configs.init()
+    overrides = [f"study={study}"]
+
+    cfg = initialize_hydra(overrides)
+
+    print(type(cfg))
+    cfg = OmegaConf.to_object(cfg)
+    pprint(OmegaConf.to_yaml(cfg, resolve=False))
+    print(type(cfg))
