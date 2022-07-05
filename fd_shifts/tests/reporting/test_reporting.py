cc49b8f7cf3edd6668185fa06fd6cbf97b63d336
Till Bungert <tillbungert@gmail.com>
2022-07-05 11:42:30 +0200

    test: remove figure tests

--- fd_shifts/tests/reporting/test_reporting.py
+++ fd_shifts/tests/reporting/test_reporting.py
@@ -4,8 +4,8 @@ import shutil
 from pathlib import Path
 
 import matplotlib
+
 matplotlib.use("agg")
-from matplotlib.testing.decorators import image_comparison
 import pytest
 
 from fd_shifts import reporting
@@ -30,7 +30,6 @@ def _check_dir_content(test_dir: Path, expected_dir: Path, snapshot):
         assert (test_dir / file).read_text() == snapshot(name=file)
 
 
-@image_comparison(baseline_images=["main_plot", "vit_v_cnn"], extensions=["png"])
 def test_reporting_blackbox(tmp_test_dir, snapshot):
     reporting.main(tmp_test_dir)
     _check_dir_content(tmp_test_dir, DATA_DIR, snapshot)
