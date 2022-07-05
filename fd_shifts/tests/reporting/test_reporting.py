90a731d73aaa0b3a0d40c2f823283b2144e98e0a
Till Bungert <tillbungert@gmail.com>
2022-07-05 12:43:23 +0200

    test: try high tol figure comparison

--- fd_shifts/tests/reporting/test_reporting.py
+++ fd_shifts/tests/reporting/test_reporting.py
@@ -4,8 +4,9 @@ import shutil
 from pathlib import Path
 
 import matplotlib
-
 matplotlib.use("agg")
+from matplotlib.testing.decorators import image_comparison
+from matplotlib import testing as mpltest
 import pytest
 
 from fd_shifts import reporting
@@ -30,6 +31,8 @@ def _check_dir_content(test_dir: Path, expected_dir: Path, snapshot):
         assert (test_dir / file).read_text() == snapshot(name=file)
 
 
+@image_comparison(baseline_images=["main_plot", "vit_v_cnn"], extensions=["png"], remove_text=True, tol=40)
 def test_reporting_blackbox(tmp_test_dir, snapshot):
+    mpltest.setup()
     reporting.main(tmp_test_dir)
     _check_dir_content(tmp_test_dir, DATA_DIR, snapshot)
