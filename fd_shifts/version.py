1e9433ae09a9db13d60829212fd0c47264d8d893
Till Bungert <tillbungert@gmail.com>
2022-07-26 18:45:26 +0200

    fix: version only works in appropriate dir

--- fd_shifts/version.py
+++ fd_shifts/version.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 import subprocess
 
 VERSION = "0.0.1"
 
 def _git_rev():
-    return subprocess.check_output(['git', 'describe', '--always']).rstrip().decode('utf8')
+    return subprocess.check_output(['git', 'describe', '--always'], cwd=Path(__file__).parent).rstrip().decode('utf8')
 
 def version():
     return f"{VERSION}+{_git_rev()}"
