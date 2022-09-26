ecb1e2bf39eb399e17dc81c2dced22d63b59bb0e
Levin Kobelke <levin.kobelke@dkfz-heidelberg.de>
2022-09-26 08:52:01 +0200

    fixup after changing the model

--- fd_shifts/models/networks/devries_network.py
+++ fd_shifts/models/networks/devries_network.py
@@ -31,6 +31,12 @@ class DeVriesAndEncoder(networks.network.Network):
     def forward_features(self, x):
         return self.encoder(x)
 
+    def head(self, x):
+        pred_class = self.classifier(x)
+        pred_confid = self.devries_net(x)
+
+        return pred_class, pred_confid
+
 
 class DeVriesNet(nn.Module):
     def __init__(self, cf):
