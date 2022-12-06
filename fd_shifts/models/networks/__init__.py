0e158afefe27d27609e64482129aa658c6bf8e75
Till Bungert <tillbungert@gmail.com>
2022-12-06 22:24:08 +0100

    refactor: remove unused networks

--- fd_shifts/models/networks/__init__.py
+++ fd_shifts/models/networks/__init__.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
 
-from abc import ABCMeta, abstractmethod
-from typing import Callable, TypeAlias, TypeVar, TYPE_CHECKING
-
-from torch import nn
+from typing import Callable, TypeAlias, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from fd_shifts import configs
@@ -22,32 +19,19 @@ def _get_network_factory() -> dict[str, NetworkFactoryType]:
     from fd_shifts.models.networks import (
         confidnet,
         devries_network,
-        dgvgg,
-        mnist_mlp,
-        mnist_small_conv,
         resnet50_imagenet,
         svhn_small_conv,
         vgg,
-        vgg16,
-        vgg_devries,
         vit,
-        zhang_network,
     )
 
     return {
         "svhn_small_conv": svhn_small_conv.SmallConv,
-        # "mnist_small_conv": mnist_small_conv.SmallConv,
-        # "mnist_mlp": mnist_mlp.MLP,
         "confidnet_and_enc": confidnet.ConfidNetAndEncoder,
         "devries_and_enc": devries_network.DeVriesAndEncoder,
         "vgg13": vgg.VGG,
         "vgg16": vgg.VGG,
-        # "vgg_old": vgg16.VGG16,
-        # "vgg_devries": vgg_devries.VGG13,
-        # "zhang_and_enc": zhang_network.ZhangAndEncoder,
-        # "zhang_backbone": zhang_network.ZhangBackbone,
         "resnet50": resnet50_imagenet.resnet50,
-        # "dgvgg": dgvgg.VGG,
         "vit": vit.ViT,
     }
 
