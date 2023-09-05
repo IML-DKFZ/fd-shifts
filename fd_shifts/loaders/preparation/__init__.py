3df696dd6ccd4dad67bba25b14a58d449178738c
Till Bungert <tillbungert@gmail.com>
2023-09-05 16:29:03 +0200

    feat: add xray preparation

--- fd_shifts/loaders/preparation/__init__.py
+++ fd_shifts/loaders/preparation/__init__.py
@@ -1,4 +1,5 @@
 from fd_shifts.loaders.preparation.dermoscopy import prepare_dermoscopy
 from fd_shifts.loaders.preparation.microscopy import prepare_rxrx1
+from fd_shifts.loaders.preparation.xray import prepare_xray
 
-__all__ = ["prepare_rxrx1", "prepare_dermoscopy"]
+__all__ = ["prepare_rxrx1", "prepare_dermoscopy", "prepare_xray"]
--- fd_shifts/loaders/prepare.py
+++ fd_shifts/loaders/prepare.py
@@ -4,7 +4,11 @@ from pathlib import Path
 
 import imageio.core.util
 
-from fd_shifts.loaders.preparation import prepare_dermoscopy, prepare_rxrx1
+from fd_shifts.loaders.preparation import (
+    prepare_dermoscopy,
+    prepare_rxrx1,
+    prepare_xray,
+)
 
 
 def ignore_warnings(*args, **kwargs):
@@ -16,7 +20,7 @@ imageio.core.util._precision_warn = ignore_warnings
 
 def add_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     parser.add_argument(
-        "--dataset", default="all", choices=("all", "rxrx1", "dermoscopy")
+        "--dataset", default="all", choices=("all", "xray", "microscopy", "dermoscopy")
     )
 
     return parser
@@ -24,8 +28,12 @@ def add_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
 
 def main(args: argparse.Namespace):
     data_dir = Path(os.getenv("DATASET_ROOT_DIR", "./data"))
-    prepare_rxrx1(data_dir)
-    prepare_dermoscopy(data_dir)
+    if args.dataset == "all" or args.dataset == "microscopy":
+        prepare_rxrx1(data_dir)
+    if args.dataset == "all" or args.dataset == "xray":
+        prepare_xray(data_dir)
+    if args.dataset == "all" or args.dataset == "dermoscopy":
+        prepare_dermoscopy(data_dir)
 
 
 if __name__ == "__main__":
