3d47c459851af61e0d1d2b8a7b7f33addb1ab3ce
Till Bungert <tillbungert@gmail.com>
2023-10-26 11:37:13 +0200

    feat: make all jobs run on smalles gpu

--- fd_shifts/experiments/cluster.py
+++ fd_shifts/experiments/cluster.py
@@ -1,22 +1,13 @@
-import argparse
-import asyncio
-import json
 import re
 import subprocess
-import sys
-import urllib.request
-from datetime import datetime
-from pathlib import Path
 from typing import Any
 
 from rich import print
-from rich.pretty import pprint
-from rich.progress import Progress
 from rich.syntax import Syntax
 
-from fd_shifts import experiments, logger
-from fd_shifts.experiments.sync import sync_to_dir_remote
-from fd_shifts.experiments.validation import ValidationResult
+from fd_shifts import experiments
+
+# -R "select[hname!='e230-dgx2-2']" \
 
 BASH_BSUB_COMMAND = r"""
 bsub -gpu num=1:j_exclusive=yes:gmem={gmem}\
@@ -24,7 +15,6 @@ bsub -gpu num=1:j_exclusive=yes:gmem={gmem}\
     -q gpu \
     -u 'till.bungert@dkfz-heidelberg.de' \
     -B {nodes} \
-    -R "select[hname!='e230-dgx2-2']" \
     -g /t974t/train \
     -J "{name}" \
     bash -li -c 'set -o pipefail; echo $LSB_JOBID && source .envrc && {command} |& tee -a "/home/t974t/logs/$LSB_JOBID.log"'
@@ -33,67 +23,6 @@ bsub -gpu num=1:j_exclusive=yes:gmem={gmem}\
 BASH_BASE_COMMAND = r"""
 _fd_shifts_exec {overrides} exp.mode={mode}
 """
-
-
-def get_jobs() -> list[dict[str, str]]:
-    with urllib.request.urlopen("http://localhost:3030/jobs") as response:
-        records: list[dict[str, str]] = json.loads(response.read())["RECORDS"]
-    return records
-
-
-def is_experiment_running(
-    experiment: ValidationResult, jobs: list[dict[str, str]]
-) -> bool:
-    _experiments = list(
-        map(
-            lambda j: j["JOB_NAME"],
-            filter(lambda j: j["STAT"] in ("RUN", "PEND", "DONE"), jobs),
-        )
-    )
-    running = (
-        str(experiment.experiment.to_path().relative_to("fd-shifts")) in _experiments
-    )
-
-    if running:
-        print(f"{experiment.experiment.to_path()} is already running")
-
-    return running
-
-
-def get_batch_size(dataset: str, model: str, mode: str):
-    match mode:
-        case "test":
-            if model == "vit":
-                return 80
-
-            if dataset in [
-                "wilds_animals",
-                "animals",
-                "wilds_animals_openset",
-                "animals_openset",
-                "breeds",
-            ]:
-                return 128
-
-            return 512
-        case "train" | "train_test":
-            match model:
-                case "vit":
-                    match dataset:
-                        case "svhn" | "svhn_openset" | "cifar10" | "breeds":
-                            return "64 +trainer.accumulate_grad_batches=2"
-                        case "wilds_animals" | "wilds_animals_openset" | "cifar100" | "super_cifar100":
-                            return "64 +trainer.accumulate_grad_batches=8"
-                case _:
-                    match dataset:
-                        case "svhn" | "svhn_openset" | "cifar10" | "cifar100" | "super_cifar100" | "breeds":
-                            return 128
-                        case "wilds_animals" | "wilds_animals_openset" | "animals" | "animals_openset":
-                            return 16
-                        case "wilds_camelyon":
-                            return 32
-        case _:
-            return 128
 
 
 def get_nodes(mode: str):
@@ -109,27 +38,39 @@ def get_gmem(mode: str, model: str):
         case "train" | "train_test":
             match model:
                 case "vit":
-                    return "33G"
+                    return "23G"
                 case _:
-                    return "33G"
+                    return "23G"
         case _:
             match model:
                 case "vit":
-                    return "33G"
+                    return "23G"
                 case _:
-                    return "33G"
+                    return "23G"
 
 
-def update_overrides(overrides: dict[str, Any]) -> dict[str, Any]:
-    if overrides.get("trainer.batch_size", -1) > 64:
-        accum = overrides["trainer.batch_size"] // 64
-        overrides["trainer.batch_size"] = 64
+def update_overrides(
+    overrides: dict[str, Any], iid_only: bool = False, mode: str = "train_test"
+) -> dict[str, Any]:
+    if mode in ["train", "train_test"] and overrides.get("trainer.batch_size", -1) > 32:
+        accum = overrides["trainer.batch_size"] // 32
+        overrides["trainer.batch_size"] = 32
         overrides["trainer.accumulate_grad_batches"] = accum
+
+    if mode in ["test"]:
+        overrides["trainer.batch_size"] = 256
+
+    if iid_only:
+        overrides["eval.query_studies.noise_study"] = []
+        overrides["eval.query_studies.in_class_study"] = []
+        overrides["eval.query_studies.new_class_study"] = []
 
     return overrides
 
 
-def submit(_experiments: list[experiments.Experiment], mode: str, dry_run: bool):
+def submit(
+    _experiments: list[experiments.Experiment], mode: str, dry_run: bool, iid_only: bool
+):
     try:
         from pssh.clients import SSHClient
         from pssh.exceptions import Timeout
@@ -142,19 +83,22 @@ def submit(_experiments: list[experiments.Experiment], mode: str, dry_run: bool)
         print("Nothing to run")
         return
 
-    client = SSHClient("odcf-worker01.inet.dkfz-heidelberg.de")
+    if not dry_run:
+        client = SSHClient("odcf-worker02.inet.dkfz-heidelberg.de")
 
     for experiment in _experiments:
         try:
-            if path := experiment.overrides().get(
-                "trainer.callbacks.training_stages.pretrained_backbone_path"
-            ):
-                sync_to_dir_remote(
-                    path.replace("${EXPERIMENT_ROOT_DIR%/}/", "fd-shifts/"),
-                    dry_run=dry_run,
-                )
+            # if path := experiment.overrides().get(
+            #     "trainer.callbacks.training_stages.pretrained_backbone_path"
+            # ):
+            #     sync_to_dir_remote(
+            #         path.replace("${EXPERIMENT_ROOT_DIR%/}/", "fd-shifts/"),
+            #         dry_run=dry_run,
+            #     )
 
-            overrides = update_overrides(experiment.overrides())
+            overrides = update_overrides(
+                experiment.overrides(), iid_only=iid_only, mode=mode
+            )
             cmd = BASH_BASE_COMMAND.format(
                 overrides=" ".join(f"{k}={v}" for k, v in overrides.items()),
                 mode=mode,
@@ -189,7 +133,7 @@ def submit(_experiments: list[experiments.Experiment], mode: str, dry_run: bool)
                 continue
 
             with client.open_shell(read_timeout=1) as shell:
-                shell.run("cd failure-detection-benchmark")
+                shell.run("cd ~/Projects/failure-detection-benchmark")
                 shell.run("source .envrc")
                 shell.run(cmd)
 
