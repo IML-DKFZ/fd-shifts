275fe9ecb290b3275f0d82d94fe1f7234d6d6bf7
Till Bungert <tillbungert@gmail.com>
2023-10-26 11:36:51 +0200

    refactor: take out experiment filter

--- fd_shifts/experiments/launcher.py
+++ fd_shifts/experiments/launcher.py
@@ -4,14 +4,13 @@ import json
 import re
 from datetime import datetime
 from pathlib import Path
-from typing import Any
+from typing import Any, Iterator
 
 import rich
 from rich.syntax import Syntax
 
 from fd_shifts import experiments, logger
 from fd_shifts.experiments.cluster import submit
-from fd_shifts.experiments.validation import ValidationResult
 
 BASH_LOCAL_COMMAND = r"""
 bash -c 'set -o pipefail; {command} |& tee -a "./logs/{log_file_name}.log"'
@@ -20,17 +19,6 @@ bash -c 'set -o pipefail; {command} |& tee -a "./logs/{log_file_name}.log"'
 BASH_BASE_COMMAND = r"""
 _fd_shifts_exec {overrides} exp.mode={mode}
 """
-
-
-def parse_validation_file(validation_file: Path) -> list[ValidationResult]:
-    with validation_file.open() as file:
-        _experiments = json.load(file)
-
-    _experiments = list(map(lambda t: ValidationResult(**t[1]), _experiments.items()))
-    for exp in _experiments:
-        exp.experiment = experiments.Experiment(**exp.experiment)
-        exp.logs = []
-    return _experiments
 
 
 async def worker(name, queue: asyncio.Queue[str]):
@@ -55,12 +43,20 @@ async def worker(name, queue: asyncio.Queue[str]):
 
 
 def update_overrides(
-    overrides: dict[str, Any], max_batch_size: int = 32
+    overrides: dict[str, Any], iid_only: bool = False, mode: str = "train_test"
 ) -> dict[str, Any]:
-    if overrides.get("trainer.batch_size", -1) > max_batch_size:
-        accum = overrides["trainer.batch_size"] // max_batch_size
-        overrides["trainer.batch_size"] = max_batch_size
+    if mode in ["train", "train_test"] and overrides.get("trainer.batch_size", -1) > 32:
+        accum = overrides["trainer.batch_size"] // 32
+        overrides["trainer.batch_size"] = 32
         overrides["trainer.accumulate_grad_batches"] = accum
+
+    if mode in ["test"]:
+        overrides["trainer.batch_size"] = 256
+
+    if iid_only:
+        overrides["eval.query_studies.noise_study"] = []
+        overrides["eval.query_studies.in_class_study"] = []
+        overrides["eval.query_studies.new_class_study"] = []
 
     return overrides
 
@@ -70,6 +66,7 @@ async def run(
     mode: str,
     dry_run: bool,
     max_batch_size: int = 32,
+    iid_only: bool = False,
 ):
     if len(_experiments) == 0:
         print("Nothing to run")
@@ -83,7 +80,7 @@ async def run(
     for experiment in _experiments:
         log_file_name = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}-{str(experiment.to_path()).replace('/', '_').replace('.','_')}"
 
-        overrides = update_overrides(experiment.overrides(), max_batch_size)
+        overrides = update_overrides(experiment.overrides(), iid_only, mode)
 
         cmd = BASH_BASE_COMMAND.format(
             overrides=" ".join(f"{k}={v}" for k, v in overrides.items()),
@@ -126,6 +123,71 @@ async def run(
     await asyncio.gather(*tasks, return_exceptions=True)
 
 
+def filter_experiments(
+    dataset: str | None,
+    dropout: int | None,
+    model: str | None,
+    backbone: str | None,
+    exclude_model: str | None,
+    run_nr: int | None,
+    rew: float | None,
+    name: str | None,
+) -> Iterator[experiments.Experiment]:
+    _experiments = experiments.get_all_experiments()
+
+    _experiments = filter(
+        lambda e: "precision_study" not in str(e.to_path()), _experiments
+    )
+
+    if dataset is not None:
+        _experiments = filter(
+            lambda experiment: experiment.dataset == dataset,
+            _experiments,
+        )
+
+    if dropout is not None:
+        _experiments = filter(
+            lambda experiment: experiment.dropout == dropout,
+            _experiments,
+        )
+    if rew is not None:
+        _experiments = filter(
+            lambda experiment: experiment.reward == rew,
+            _experiments,
+        )
+    if run_nr is not None:
+        _experiments = filter(
+            lambda experiment: experiment.run == run_nr,
+            _experiments,
+        )
+
+    if model is not None:
+        _experiments = filter(
+            lambda experiment: experiment.model == model,
+            _experiments,
+        )
+
+    if backbone is not None:
+        _experiments = filter(
+            lambda experiment: experiment.backbone == backbone,
+            _experiments,
+        )
+
+    if exclude_model is not None:
+        _experiments = filter(
+            lambda experiment: experiment.model != exclude_model,
+            _experiments,
+        )
+
+    if name is not None:
+        _experiments = filter(
+            lambda experiment: str(experiment.to_path()) == name,
+            _experiments,
+        )
+
+    return _experiments
+
+
 def launch(
     dataset: str | None,
     dropout: int | None,
@@ -139,75 +201,18 @@ def launch(
     cluster: bool,
     name: str | None,
     max_batch_size: int,
+    iid_only: bool,
 ):
-    _experiments = experiments.get_all_experiments()
-
-    _experiments = list(
-        filter(lambda e: "precision_study" not in str(e.to_path()), _experiments)
+    _experiments = filter_experiments(
+        dataset,
+        dropout,
+        model,
+        backbone,
+        exclude_model,
+        run_nr,
+        rew,
+        name,
     )
-
-    if dataset is not None:
-        _experiments = list(
-            filter(
-                lambda experiment: experiment.dataset == dataset,
-                _experiments,
-            )
-        )
-
-    if dropout is not None:
-        _experiments = list(
-            filter(
-                lambda experiment: experiment.dropout == dropout,
-                _experiments,
-            )
-        )
-    if rew is not None:
-        _experiments = list(
-            filter(
-                lambda experiment: experiment.reward == rew,
-                _experiments,
-            )
-        )
-    if run_nr is not None:
-        _experiments = list(
-            filter(
-                lambda experiment: experiment.run == run_nr,
-                _experiments,
-            )
-        )
-
-    if model is not None:
-        _experiments = list(
-            filter(
-                lambda experiment: experiment.model == model,
-                _experiments,
-            )
-        )
-
-    if backbone is not None:
-        _experiments = list(
-            filter(
-                lambda experiment: experiment.backbone == backbone,
-                _experiments,
-            )
-        )
-
-    if exclude_model is not None:
-        _experiments = list(
-            filter(
-                lambda experiment: experiment.model != exclude_model,
-                _experiments,
-            )
-        )
-
-    if name is not None:
-        _experiments = list(
-            filter(
-                lambda experiment: str(experiment.to_path()) == name,
-                _experiments,
-            )
-        )
-
     print("Launching:")
     for exp in map(
         lambda exp: str(exp.to_path()),
@@ -216,12 +221,12 @@ def launch(
         rich.print(exp)
 
     if cluster:
-        submit(_experiments, mode, dry_run)
+        submit(_experiments, mode, dry_run, iid_only)
     else:
-        asyncio.run(run(_experiments, mode, dry_run, max_batch_size))
+        asyncio.run(run(_experiments, mode, dry_run, max_batch_size, iid_only))
 
 
-def add_arguments(parser: argparse.ArgumentParser):
+def add_filter_arguments(parser: argparse.ArgumentParser):
     parser.add_argument("--dataset", default=None, type=str)
     parser.add_argument("--dropout", default=None, type=int, choices=(0, 1))
     parser.add_argument(
@@ -237,6 +242,13 @@ def add_arguments(parser: argparse.ArgumentParser):
 
     parser.add_argument("--run", default=None, type=int)
     parser.add_argument("--reward", default=None, type=float)
+    parser.add_argument("--name", default=None, type=str)
+
+    return parser
+
+
+def add_arguments(parser: argparse.ArgumentParser):
+    add_filter_arguments(parser)
     parser.add_argument("--dry-run", action="store_true")
     parser.add_argument(
         "--mode",
@@ -244,8 +256,8 @@ def add_arguments(parser: argparse.ArgumentParser):
         choices=("test", "train", "train_test", "analysis"),
     )
     parser.add_argument("--cluster", action="store_true")
+    parser.add_argument("--iid-only", action="store_true")
 
-    parser.add_argument("--name", default=None, type=str)
     parser.add_argument("--max-batch-size", default=32, type=int)
 
     return parser
@@ -267,6 +279,7 @@ def main(args):
         cluster=args.cluster,
         name=args.name,
         max_batch_size=args.max_batch_size,
+        iid_only=args.iid_only,
     )
 
 
